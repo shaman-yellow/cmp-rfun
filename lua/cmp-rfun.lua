e15ad5be35fddf3eda963160c57fbc6a1e4fa6e7
shaman-yellow <shaman.yellow@foxmail.com>
2024-05-30 10:39:27 +0800

    Thu May 30 10:39:27 AM CST 2024

--- lua/cmp-rfun.lua
+++ lua/cmp-rfun.lua
@@ -14,25 +14,45 @@ function source:get_debug_name()
   return 'cmp-rfun'
 end
 
+local function get_script_dir()
+  local info = debug.getinfo(1, 'S')
+  local script_path = info.source:sub(2)
+  return script_path:match("(.*[/\\])")
+end
+
 function source:complete(params, callback)
-  local bufnr = vim.api.nvim_get_current_buf()
-  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
   local candidates = {}
   local seen = {}
+  local function add_candidate(label)
+    if not seen[label] then
+      table.insert(candidates, {
+        label = label,
+        kind = require('cmp.types').lsp.CompletionItemKind.Function,
+        insertText = string.format("%s($1)", label),
+        insertTextFormat = require('cmp.types').lsp.InsertTextFormat.Snippet,
+      })
+      seen[label] = true
+    end
+  end
+
+  -- Get the directory of the current script and build the relative path
+  local script_dir = get_script_dir()
+  local filename = script_dir .. 'functions.txt'
+
+  -- Load external text
+  for line in io.lines(filename) do
+    add_candidate(line)
+  end
+
+  -- Load candidates from the current buffer
+  local bufnr = vim.api.nvim_get_current_buf()
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
   local gmatch = string.gmatch
 
   for _, line in ipairs(lines) do
     for package, func in gmatch(line, '([%w_]+)::([%w_]+)') do
       local label = package .. '::' .. func .. '()'
-      if not seen[label] then
-        table.insert(candidates, {
-          label = label,
-          kind = require('cmp.types').lsp.CompletionItemKind.Function,
-          insertText = package .. '::' .. func .. '($1)',
-          insertTextFormat = require('cmp.types').lsp.InsertTextFormat.Snippet,
-        })
-        seen[label] = true
-      end
+      add_candidate(label)
     end
   end
 
